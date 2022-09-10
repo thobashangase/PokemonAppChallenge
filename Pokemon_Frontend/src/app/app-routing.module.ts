138586499510d8297809a95d3f4fd76e95915666
Thoba Shangase <shangasett@outlook.com>
2022-09-10 12:13:03 +0200

    List and details pages working

--- Pokemon_Frontend/src/app/app-routing.module.ts
+++ Pokemon_Frontend/src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
+import { HomeComponent } from './home/home.component';
+import { DetailsComponent } from './details/details.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'pokemon', component: DetailsComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
